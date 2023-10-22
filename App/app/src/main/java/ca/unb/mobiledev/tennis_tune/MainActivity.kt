390d297e53e05a98b9f2bf353f2fe87ed4bae0d6
t-hauser <145395845+t-hauser@users.noreply.github.com>
2023-10-22 16:22:45 -0300

    Added audio recording and visualization based on the current state of the project in main. Used the code from the following project to implement it: https://github.com/romerojhh/androidAudioVisualizer. Visualization is not shown yet, due to some error I could not find yet. Do you have an idea how to fix it?

--- App/app/src/main/java/ca/unb/mobiledev/tennis_tune/MainActivity.kt
+++ App/app/src/main/java/ca/unb/mobiledev/tennis_tune/MainActivity.kt
@@ -1,22 +1,47 @@
+//Source for visualizer and audio recording code: https://github.com/romerojhh/androidAudioVisualizer
 package ca.unb.mobiledev.tennis_tune
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
+import android.media.audiofx.Visualizer
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import ca.unb.mobiledev.tennis_tune.databinding.ActivityMainBinding
+import ca.unb.mobiledev.tennis_tune.theme.ApiexplorationTheme
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private val MY_PERMISSIONS_RECORD_AUDIO = 1
+    private val VISUALIZER_HEIGHT_DIP = 50f
+    private var mVisualizer: Visualizer? = null
+    private var mLinearLayout: LinearLayout? = null
+    private var mVisualizerView: VisualizerView? = null
+    private var mStatusTextView: TextView? = null
+    private val job = Job()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +67,17 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        setupVisualizerFxAndUI()
+        checkRecordAudioPermission()
+
+        mStatusTextView = TextView(this)
+        mLinearLayout = LinearLayout(this)
+        mLinearLayout!!.orientation = LinearLayout.VERTICAL
+        mLinearLayout!!.addView(mStatusTextView)
+        setContentView(mLinearLayout)
+        mStatusTextView!!.text = "Playing audio..."
+        startAudioCapture()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -53,5 +89,95 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun setupVisualizerFxAndUI() {
+        // Create a VisualizerView (defined below), which will render the simplified audio
+        // wave form to a Canvas.
+        mVisualizerView = VisualizerView(this)
+        mVisualizerView!!.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.FILL_PARENT,
+            (this.VISUALIZER_HEIGHT_DIP * resources.displayMetrics.density).toInt()
+        )
+        mLinearLayout?.addView(mVisualizerView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (isFinishing) {
+            mVisualizer?.release()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startAudioCapture() {
+        val audioSource = MediaRecorder.AudioSource.MIC
+        val sampleRateInHz = 44100
+        val channelConfig = AudioFormat.CHANNEL_IN_MONO
+        val audioFormat = AudioFormat.ENCODING_PCM_8BIT
+        val bufferSizeInBytes = AudioRecord.getMinBufferSize(sampleRateInHz, channelConfig, audioFormat)
+        val audioRecord = AudioRecord(audioSource, sampleRateInHz, channelConfig, audioFormat, bufferSizeInBytes)
+        val buffer = ByteArray(bufferSizeInBytes)
+        val scope = CoroutineScope(Dispatchers.IO + job)
+
+        scope.launch {
+            // Do something with the recorded audio data
+            audioRecord.startRecording()
+            while (isActive) {
+                val readSize = audioRecord.read(buffer, 0, buffer.size, AudioRecord.READ_BLOCKING)
+
+                System.out.println(buffer.contentToString())
+                mVisualizerView?.updateVisualizer(buffer.sliceArray(0 until readSize))
+            }
+            audioRecord.stop()
+        }
+    }
+
+    private fun checkRecordAudioPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission has not been granted yet, request it from the user
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                MY_PERMISSIONS_RECORD_AUDIO
+            )
+        } else {
+            // Permission has already been granted, proceed with audio capture
+            startAudioCapture()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == MY_PERMISSIONS_RECORD_AUDIO) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted, proceed with audio capture
+                startAudioCapture()
+            } else {
+                // Permission has been denied, show a message to the user or handle the error
+                Toast.makeText(this, "Audio capture permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ApiexplorationTheme {
+        Greeting("Android")
     }
 }
