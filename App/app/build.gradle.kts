7c1c54ee3f3ffa6b9b17ed3ad92e3195c428b4b4
low-earth-orbit <5917188+low-earth-orbit@users.noreply.github.com>
2023-10-24 13:02:17 -0300

    Update VisualizerView.kt

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android");
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -56,4 +56,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.github.wendykierp:JTransforms:3.1")
 }
--- App/app/src/main/java/ca/unb/mobiledev/tennis_tune/VisualizerView.kt
+++ App/app/src/main/java/ca/unb/mobiledev/tennis_tune/VisualizerView.kt
@@ -3,13 +3,27 @@ package ca.unb.mobiledev.tennis_tune
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
 import android.view.View
+import org.jtransforms.fft.FloatFFT_1D
+import kotlin.math.absoluteValue
 
 class VisualizerView : View {
     private val paint = Paint()
     private var amplitudes: ByteArray = ByteArray(0)
     private var isAudioInputAvailable = false
+    private var fft = FloatFFT_1D(1024) // Assuming 1024 samples
+    private var magnitudes = FloatArray(512) // Magnitudes (after FFT)
+    private var maxFrequency: Float = 0f
+    private val handler = Handler(Looper.getMainLooper())
+    private val resetFrequencyRunnable = Runnable {
+        maxFrequency = 0f
+        invalidate()
+    }
+    private val sampleRate =
+        44100  // For example, typical CD quality audio uses a sample rate of 44.1 kHz
 
     constructor(context: Context) : super(context) {
         init()
@@ -20,27 +34,71 @@ class VisualizerView : View {
     }
 
     private fun init() {
-        paint.color =
-            resources.getColor(android.R.color.holo_blue_light, null) // Updated way to getColor
+        paint.color = resources.getColor(android.R.color.white, null)
         paint.style = Paint.Style.FILL
     }
 
     fun updateVisualizer(newAmplitudes: ByteArray) {
         amplitudes = newAmplitudes
-        invalidate()  // Request a redraw.
+
+        // Find the index with the maximum amplitude after FFT
+        val maxIndex =
+            amplitudes.indices.maxByOrNull { (amplitudes[it].toInt() and 0xFF).absoluteValue } ?: -1
+        if (maxIndex != -1) {
+            // Convert index to frequency: frequency = index * sampleRate / FFT_Size
+            // NOTE: You might need to adjust this based on your specific FFT implementation details
+            maxFrequency =
+                (maxIndex * sampleRate / (2 * amplitudes.size)).toFloat() // The '2' assumes the FFT size is twice the amplitude array size
+        }
+
+        invalidate()  // Request a redraw
+
+        // Schedule the reset after your desired interval (e.g., 5 seconds)
+//        handler.removeCallbacks(resetFrequencyRunnable)
+//        handler.postDelayed(resetFrequencyRunnable, 10000)
+    }
+
+
+    private fun computeFFT(amplitudes: ByteArray) {
+        // Convert byte array to float array
+        val floatData = FloatArray(1024) { i -> amplitudes.getOrElse(i) { 0 }.toFloat() }
+
+        // Apply FFT
+        fft.realForward(floatData)
+
+        // Compute magnitude for each frequency bin
+        for (i in 0 until magnitudes.size) {
+            val real = floatData[2 * i]
+            val imaginary = floatData[2 * i + 1]
+            magnitudes[i] = kotlin.math.sqrt(real * real + imaginary * imaginary)
+        }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        if (isAudioInputAvailable && amplitudes.isNotEmpty()) {
-            val barWidth = width / amplitudes.size.toFloat()
-            amplitudes.forEachIndexed { index, amplitude ->
-                val x = index * barWidth
-                val normalizedAmplitude = (amplitude.toInt() + 128) / 256f
-                val heightAmplitude = normalizedAmplitude * height
-                canvas.drawRect(x, height - heightAmplitude, x + barWidth, height.toFloat(), paint)
+        if (isAudioInputAvailable && magnitudes.isNotEmpty()) {
+            val numberOfBars = 32 // for example, which is a fraction of the original 512
+            val step = magnitudes.size / numberOfBars
+            val barWidth = width / numberOfBars.toFloat()
+            val scale = 0.5f // Adjust this value to control the vertical scale
+
+            for (i in 0 until numberOfBars) {
+                val index = i * step
+                val magnitude = magnitudes[index]
+                val x = i * barWidth
+                val normalizedMagnitude = magnitude / (256f * scale) // Adjusted normalization
+                val heightMagnitude = normalizedMagnitude * height
+                canvas.drawRect(
+                    x,
+                    0f,
+                    x + barWidth,
+                    heightMagnitude,
+                    paint
+                ) // Drawing from top to magnitude height
             }
+//            paint.textSize = 40f // Adjust text size as needed
+//            canvas.drawText("Max Frequency: ${maxFrequency / 1000} kHz", 10f, 50f, paint)
         }
     }
 
