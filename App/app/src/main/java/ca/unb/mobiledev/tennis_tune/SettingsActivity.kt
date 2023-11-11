e111cad81a7d3dbb5c2f4b1eae8937d5feea47c6
low-earth-orbit <5917188+low-earth-orbit@users.noreply.github.com>
2023-11-11 02:33:49 -0400

    Add input validation to settings page

--- App/app/src/main/java/ca/unb/mobiledev/tennis_tune/SettingsActivity.kt
+++ App/app/src/main/java/ca/unb/mobiledev/tennis_tune/SettingsActivity.kt
@@ -2,18 +2,21 @@ package ca.unb.mobiledev.tennis_tune
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MenuItem
-import android.widget.TextView
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
 import ca.unb.mobiledev.tennis_tune.databinding.SettingsPageBinding
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var binding: SettingsPageBinding
-
     private lateinit var sharedPreferences: SharedPreferences
-    private var mVisualizerView: VisualizerView? = null
-    private var frequencyTextView: TextView? = null
+    private var originalDisplayUnit: String? = null
+    private var originalRacquetHeadSize: String? = null
+    private var originalStringMassDensity: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,41 +32,105 @@ class SettingsActivity : AppCompatActivity() {
 
         setupListeners()
         loadSettings()
+        setupInputValidation()
     }
 
     private fun setupListeners() {
-        binding.rgDisplayUnit.setOnCheckedChangeListener { _, checkedId ->
-            sharedPreferences.edit()
-                .putString("DISPLAY_UNIT", if (checkedId == R.id.rb_lb) "lb" else "kg").apply()
+        binding.buttonSave.setOnClickListener {
+            if (validateInputs()) {
+                saveSettings()
+                Toast.makeText(this, getString(R.string.settings_saved), Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, getString(R.string.invalid_inputs), Toast.LENGTH_SHORT).show()
+            }
         }
+    }
 
-        binding.etRacquetHeadSize.addTextChangedListener {
-            sharedPreferences.edit().putString("RACQUET_HEAD_SIZE", it.toString()).apply()
-        }
+    private fun loadSettings() {
+        originalDisplayUnit = sharedPreferences.getString("DISPLAY_UNIT", "lb")
+        originalRacquetHeadSize = sharedPreferences.getString("RACQUET_HEAD_SIZE", "100")
+        originalStringMassDensity = sharedPreferences.getString("STRING_MASS_DENSITY", "1.53")
 
-        binding.etStringMassDensity.addTextChangedListener {
-            sharedPreferences.edit().putString("STRING_MASS_DENSITY", it.toString()).apply()
-        }
+        binding.rbLb.isChecked = (originalDisplayUnit == "lb")
+        binding.rbKg.isChecked = (originalDisplayUnit == "kg")
+        binding.etRacquetHeadSize.setText(originalRacquetHeadSize)
+        binding.etStringMassDensity.setText(originalStringMassDensity)
+    }
 
-        binding.buttonBack.setOnClickListener {
-            onSettingsChanged()
+    private fun setupInputValidation() {
+        val etHeadSize = findViewById<EditText>(R.id.et_racquet_head_size)
+        val etStringMassDensity = findViewById<EditText>(R.id.et_string_mass_density)
+
+        binding.etRacquetHeadSize.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                val headSize = s.toString().toDoubleOrNull()
+                if (headSize == null || !isValidHeadSize(headSize)) {
+                    etHeadSize.error = getString(R.string.supported_head_size)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        })
+
+        binding.etStringMassDensity.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                val density = s.toString().toDoubleOrNull()
+                if (density == null || !isValidStringMassDensity(density)) {
+                    etStringMassDensity.error =
+                        getString(R.string.string_mass_density_accepted_range)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        })
+    }
+
+    private fun validateInputs(): Boolean {
+        val headSize = binding.etRacquetHeadSize.text.toString().toDoubleOrNull()
+        val stringDensity = binding.etStringMassDensity.text.toString().toDoubleOrNull()
+
+        return when {
+            headSize == null || !isValidHeadSize(headSize) -> {
+                binding.etRacquetHeadSize.error = getString(R.string.supported_head_size)
+                false
+            }
+
+            stringDensity == null || !isValidStringMassDensity(stringDensity) -> {
+                binding.etStringMassDensity.error =
+                    getString(R.string.string_mass_density_accepted_range)
+                false
+            }
+
+            else -> true
         }
+    }
+
+    private fun isValidHeadSize(size: Double): Boolean {
+        return size in 85.0..130.0
+    }
+
+    private fun isValidStringMassDensity(density: Double): Boolean {
+        return density in 0.5..3.0
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle the Up/Home button
         if (item.itemId == android.R.id.home) {
-            onSettingsChanged()
+            if (hasUnsavedChanges()) {
+                AlertDialog.Builder(this)
+                    .setMessage(getString(R.string.unsaved_changes_message))
+                    .setPositiveButton(getString(R.string.discard)) { _, _ -> onBackPressedDispatcher.onBackPressed() }
+                    .setNegativeButton(getString(R.string.cancel), null)
+                    .show()
+            } else {
+                finish()
+            }
             return true
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    private fun onSettingsChanged() {
-        saveSettings()
-        mVisualizerView?.resetFrequencies()
-        frequencyTextView?.text = this.getString(R.string.text_detecting)
-        finish()
     }
 
     private fun saveSettings() {
@@ -84,18 +151,13 @@ class SettingsActivity : AppCompatActivity() {
         editor.apply()
     }
 
-    private fun loadSettings() {
-        when (sharedPreferences.getString("DISPLAY_UNIT", "lb")) {
-            "lb" -> binding.rbLb.isChecked = true
-            "kg" -> binding.rbKg.isChecked = true
-        }
+    private fun hasUnsavedChanges(): Boolean {
+        val currentDisplayUnit = if (binding.rbLb.isChecked) "lb" else "kg"
+        val currentRacquetHeadSize = binding.etRacquetHeadSize.text.toString()
+        val currentStringMassDensity = binding.etStringMassDensity.text.toString()
 
-        binding.etRacquetHeadSize.setText(sharedPreferences.getString("RACQUET_HEAD_SIZE", "100"))
-        binding.etStringMassDensity.setText(
-            sharedPreferences.getString(
-                "STRING_MASS_DENSITY",
-                "1.50"
-            )
-        )
+        return currentDisplayUnit != originalDisplayUnit ||
+                currentRacquetHeadSize != originalRacquetHeadSize ||
+                currentStringMassDensity != originalStringMassDensity
     }
 }
