f040c70075800a777d5365a2d0ca15551a8a87c2
Qihao Guo <qhguo.225@gmail.com>
2023-11-23 00:55:09 -0400

    Add dependencies for rooms

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -59,4 +60,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.wendykierp:JTransforms:3.1")
+    implementation("androidx.room:room-runtime:2.5.2")
+    annotationProcessor("androidx.room:room-compiler:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+
 }
+
